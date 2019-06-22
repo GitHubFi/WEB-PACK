f3488a169a539967c7c3d18cb475dac8791818f4
Meraj <ashfaqahmed1618@gmail.com>
2019-06-22 12:05:09 -0700

    add second name

--- src/component/App.js
+++ src/component/App.js
@@ -4,7 +4,7 @@ class App extends Component {
     render () {
         return (
             <div>
-                <h1>Hello World  Meraj</h1>
+                <h1>Hello World  Meraj sahir</h1>
             </div>
         )
     }
